76ca837755bbc597d9e088e3b0362ba6668a774a
Maurice Codespoti Goodman <goodman.maurice@gmail.com>
2026-06-11 14:08:02 -0400

    dbSEABED phi estimates

--- R/spatial.R
+++ R/spatial.R
@@ -55,17 +55,45 @@ get_bathymetry <- function() {
 }
 
 #' @title Log sediment grain size (Krumbein phi scale) for the EBS region
-#' @description Estimates from the EBSSED database interpolated / extrapolated using
-#' ordinary kriging with an exponential fit to the empirical semi-variogram.
-#' @return A "stars" object
+#' @param source "dbSEABED" or "EBSSED" (see details)
+#' @param type "surface" for pre-interpolated surface, or "points" for underlying data
+#' @description
+#' Maps of mean sediment grain size (on the Krumbein phi scale) from either the dbSEABED database
+#' (Jenkins et al. 2025) or the EBSSED database (Richwine et al. 2018). 
+#' @details
+#' dbSEABED estimates are newer and based on data that include the NBS domain; NBS estimates from the EBSSED data are extrapolation. These are pre-processed files using inverse distance weighting for 
+#' dbSEABED or ordinary Kriging for EBSSED; all original point data (filtered to non-missing phi values) from dbSEABED and EBSSED are available as point data if a different interpolation method is preferred. 
+#' 
+#' For further detail on dbSEABED fields, see documentation of the _grz_OtherParams_idw3d.gdb and the _xyPOINTS_FNLf_VectorData.gdb datasets from Jenkins et al. (2025).
+#' @return A "stars" object (if type = "surface") or "sf" object (if type = "points")
+#' @source Jenkins, C. J., McConnaughey, R. A., and Intelmann, S. S. 2025. Documentation for multi-parameter characterization of seafloor substrates in the U.S. EEZ off Alaska. U.S. Department of Commerce, NOAA Technical Memorandum NMFS-AFSC-495, 64 p.
 #' @source Richwine, K. A. et al. 2018. Surficial sediments of the eastern Bering Sea continental shelf: EBSSED-2 database documentation. – US Dept Comm, NOAA Tech Mem. NMFS-AFSC-377
+#' @source Smith, K. R and McConnaughey, R. A.; 1999: EBSSED database-Surficial sediments of the eastern Bering Sea continental shelf. NOAA National Centers for Environmental Information.
 #' @export
-get_sediment <- function() {
+get_sediment <- function(source = c("dbSEABED", "EBSSED"), type = c("surface", "points")) {
 
-  phi <- stars::read_stars(system.file(package = "BeringSeaData", "GIS", "phi.grd"))
-  phi <- phi |> stars::st_warp(crs = "+proj=longlat +datum=WGS84 +no_defs")
-  names(phi) <- "phi"
-  phi
+  source <- match.arg(source)
+  type <- match.arg(type)
+
+  if (type == "surface") {
+    
+    data <- source |> switch(
+      dbSEABED = stars::read_ncdf(system.file(package = "BeringSeaData", "GIS", "dbSEABED_phi.nc"), var = "Band1"),
+      EBSSED = stars::read_stars(system.file(package = "BeringSeaData", "GIS", "EBSSED_phi.grd"))
+    )
+    
+    names(data) <- "phi"
+    
+  } else {
+
+    data <- source |> switch(
+      dbSEABED = sf::read_sf(system.file(package = "BeringSeaData", "GIS", "dbSEABED_phi_pts")), 
+      EBSSED = sf::read_sf(system.file(package = "BeringSeaData", "GIS", "EBSSED_phi_pts"))
+    )
+
+  }
+  
+  data
 
 }
 
